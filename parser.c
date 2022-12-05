f64419beb33f1e27a5c1d42f8911a315ed753582
reyna <reynaldo.martinez1030@gmail.com>
2022-12-05 06:53:17 -0500

    update

--- parser.c
+++ parser.c
@@ -38,6 +38,8 @@ void term();
 void factor();
 int rel_operator();
 
+int getType();
+
 instruction *parse(int code_flag, int table_flag, lexeme *list)
 {
 	tokens = list;
@@ -45,69 +47,89 @@ instruction *parse(int code_flag, int table_flag, lexeme *list)
 	code = calloc(ARRAY_SIZE, sizeof(instruction));
 
 	program();
+	if (error == -1)
+		return;
 
 	code[code_index].op = -1;
 
-	if (table_flag) {
+	if (table_flag)
+	{
 		print_symbol_table();
 	}
 
-	if (code_flag) {
+	if (code_flag)
+	{
 		print_assembly_code();
 	}
-	
 }
 
-void program() {
+void program()
+{
 	emit(7, 0, 0);
 	add_symbol(3, "main", 0, 0, 0);
 	level = -1;
 	block();
 
-	if(error == -1) return;
+	if (error == -1)
+		return;
 
 	// Parser error 1 (missing .)
-	if (tokens[token_index].type != period) {
+	if (tokens[token_index].type != period)
+	{
 		print_parser_error(1, 0);
 		error = 1;
 	}
 
-	for (int i = 0; i < code_index; i++) {
-		if (code[i].op == 5) {
-			if (code[i].m == 0) {
+	for (int i = 0; i < code_index; i++)
+	{
+		if (code[i].op == 5)
+		{
+			if (code[i].m == 0)
+			{
 				// Error 21
-				print_parser_error(21,0);
+				print_parser_error(21, 0);
 			}
 
 			code[i].m = table[code[i].m].address;
 		}
 	}
-
-	
 }
 
-void block() {
+void block()
+{
 	level++;
+
 	declaractions();
+	if (error == -1)
+		return;
+
 	statement();
+	if (error == -1)
+		return;
+
 	mark();
 	level--;
 }
 
-void declaractions() {
+void declaractions()
+{
 	int vars_declared = 0;
 
-	while (tokens[token_index].type == (keyword_const || keyword_var || keyword_procedure)) {
+	while (tokens[token_index].type == (keyword_const || keyword_var || keyword_procedure))
+	{
 
-		if (tokens[token_index].type == keyword_const) {
+		if (tokens[token_index].type == keyword_const)
+		{
 			const_declaration();
 		}
 
-		else if (tokens[token_index].type == keyword_var) {
+		else if (tokens[token_index].type == keyword_var)
+		{
 			vars_declared = var_declaration(vars_declared);
 		}
 
-		else if (tokens[token_index].type == keyword_procedure) {
+		else if (tokens[token_index].type == keyword_procedure)
+		{
 			proc_declaration();
 		}
 	}
@@ -115,81 +137,90 @@ void declaractions() {
 	emit(7, 0, vars_declared + 3);
 }
 
-void const_declaration() {
-	
+void const_declaration()
+{
 }
 
-int var_declaration(int number_of_variables) {
+int var_declaration(int number_of_variables)
+{
 	return 0;
 }
 
-void condition() {
+void condition()
+{
 	int type;
 
 	expression();
-	if (error == -1) return;
+	if (error == -1)
+		return;
 
 	type = tokens[token_index].type;
 
 	// missing relational operator
 	// need to change this logic around to avoid looking like his
-	if (type != equal_to && type != not_equal_to && type != less_than && type != less_than_or_equal_to && type != greater_than && type != greater_than_or_equal_to) {
+	if (type != equal_to && type != not_equal_to && type != less_than && type != less_than_or_equal_to && type != greater_than && type != greater_than_or_equal_to)
+	{
 		print_parser_error(17, 0);
 
 		// im not sure why this is here. why check identifier number and left parenthesis
 		if (type == identifier || type == number || type == left_parenthesis)
 			error = 1;
-		
-		else {
+
+		else
+		{
 			error = -1;
 			return;
 		}
-	} 
+	}
 
-	else 
+	else
 		token_index++;
-	
 
 	expression();
-	if (error == -1) return;
+	if (error == -1)
+		return;
 
-	switch (type) {
-		case equal_to:
-			emit(OPR, 0, EQL);
-			break;
+	switch (type)
+	{
+	case equal_to:
+		emit(OPR, 0, EQL);
+		break;
 
-		case not_equal_to:
-			emit(OPR, 0, NEQ);
-			break;
+	case not_equal_to:
+		emit(OPR, 0, NEQ);
+		break;
 
-		case less_than:
-			emit(OPR, 0, LSS);
-			break;
+	case less_than:
+		emit(OPR, 0, LSS);
+		break;
 
-		case greater_than:
-			emit(OPR, 0, GTR);
-			break;
+	case greater_than:
+		emit(OPR, 0, GTR);
+		break;
 
-		case greater_than_or_equal_to:
-			emit(OPR, 0, GEQ);
-			break;
-		
-		default:
-			emit(OPR, 0, -1);
-			break;
+	case greater_than_or_equal_to:
+		emit(OPR, 0, GEQ);
+		break;
+
+	default:
+		emit(OPR, 0, -1);
+		break;
 	}
 }
 
-void expression() {
+void expression()
+{
 	// int type;
 
 	term();
 
-	if (error == -1) return; // found stopping error
+	if (error == -1)
+		return; // found stopping error
 
 	// type = tokens[token_index].type;
 
-	while (tokens[token_index].type == plus || tokens[token_index].type == minus) {
+	while (tokens[token_index].type == plus || tokens[token_index].type == minus)
+	{
 		token_index++;
 		term();
 
@@ -202,15 +233,18 @@ void expression() {
 	}
 }
 
-void term() {
+void term()
+{
 	// int type;
 
 	factor();
-	if (error == -1) return; // found stopping error
+	if (error == -1)
+		return; // found stopping error
 
 	// type = tokens[token_index].type;
 
-	while (tokens[token_index].type == times || tokens[token_index].type == division) {
+	while (tokens[token_index].type == times || tokens[token_index].type == division)
+	{
 		token_index++;
 
 		factor();
@@ -224,30 +258,35 @@ void term() {
 	}
 }
 
-void factor() {
+void factor()
+{
 	int type;
 	int const_idx, var_idx;
 
-	if (tokens[token_index].type == identifier) {
+	if (tokens[token_index].type == identifier)
+	{
 		const_idx = find_symbol(tokens[token_index].identifier_name, 1);
 		var_idx = find_symbol(tokens[token_index].identifier_name, 2);
 
-		if (const_idx == var_idx) {
+		if (const_idx == var_idx)
+		{
 
-			if (find_symbol(tokens[token_index].identifier_name, 1) == -1) {
+			if (find_symbol(tokens[token_index].identifier_name, 1) == -1)
+			{
 				print_parser_error(8, 5);
 				error = 1;
 			}
 
-			else {
-				print_parser_error(18,0);
+			else
+			{
+				print_parser_error(18, 0);
 				error = 1;
 			}
 
 			emit(LOD, -1, -1);
 		}
 
-		else 
+		else
 		{
 			if (const_idx == -1)
 				emit(LOD, level - table[var_idx].level, table[var_idx].address);
@@ -255,14 +294,15 @@ void factor() {
 				emit(LIT, 0, table[const_idx].value);
 			else if (table[const_idx].level > table[var_idx].level)
 				emit(LIT, 0, table[const_idx].value);
-			else 
+			else
 				emit(LOD, level - table[var_idx].level, table[var_idx].address);
-			
+
 			token_index++;
 		}
 	}
 
-	else if (tokens[token_index].type == number) {
+	else if (tokens[token_index].type == number)
+	{
 		emit(LIT, 0, tokens[token_index].number_value);
 		token_index++;
 	}
@@ -272,8 +312,53 @@ void factor() {
 		token_index++;
 
 		expression();
-		if (error == -1) return;
+		if (error == -1)
+			return;
+
+		if (tokens[token_index].type != right_parenthesis)
+		{
+			print_parser_error(19, 0);
+			type = tokens[token_index].type;
+
+			if (type == times || type == division || type == minus || type == period || type == right_curly_brace || type == semicolon ||
+				type == keyword_end || type == equal_to || type == not_equal_to || type == less_than || type == less_than_or_equal_to ||
+				type == greater_than || type == greater_than_or_equal_to || type == keyword_then || type == keyword_do)
+				error = 1;
+
+			else
+			{
+				error = -1;
+				return;
+			}
+		}
+
+		else
+			token_index++;
 	}
+
+	else
+	{
+		print_parser_error(20, 0);
+
+		type = tokens[token_index].type;
+
+		// sometimes non-stopping
+		if (type == times || type == division || type == minus || type == period || type == right_curly_brace || type == semicolon ||
+			type == keyword_end || type == equal_to || type == not_equal_to || type == less_than || type == less_than_or_equal_to ||
+			type == greater_than || type == greater_than_or_equal_to || type == keyword_then || type == keyword_do)
+			error = 1;
+
+		else
+		{
+			error = -1;
+			return;
+		}
+	}
+}
+
+int getType()
+{
+	return tokens[token_index].type;
 }
 
 int find_symbol(char name[], int kind)
@@ -294,7 +379,6 @@ int find_symbol(char name[], int kind)
 	}
 	return max_idx;
 }
-
 
 void emit(int op, int l, int m)
 {
@@ -341,145 +425,144 @@ void print_parser_error(int error_code, int case_code)
 {
 	switch (error_code)
 	{
-		case 1 :
-			printf("Parser Error 1: missing . \n");
+	case 1:
+		printf("Parser Error 1: missing . \n");
+		break;
+	case 2:
+		switch (case_code)
+		{
+		case 1:
+			printf("Parser Error 2: missing identifier after keyword const\n");
 			break;
-		case 2 :
-			switch (case_code)
-			{
-				case 1 :
-					printf("Parser Error 2: missing identifier after keyword const\n");
-					break;
-				case 2 :
-					printf("Parser Error 2: missing identifier after keyword var\n");
-					break;
-				case 3 :
-					printf("Parser Error 2: missing identifier after keyword procedure\n");
-					break;
-				case 4 :
-					printf("Parser Error 2: missing identifier after keyword call\n");
-					break;
-				case 5 :
-					printf("Parser Error 2: missing identifier after keyword read\n");
-					break;
-				case 6 :
-					printf("Parser Error 2: missing identifier after keyword def\n");
-					break;
-				default :
-					printf("Implementation Error: unrecognized error code\n");
-			}
+		case 2:
+			printf("Parser Error 2: missing identifier after keyword var\n");
 			break;
-		case 3 :
-			printf("Parser Error 3: identifier is declared multiple times by a procedure\n");
+		case 3:
+			printf("Parser Error 2: missing identifier after keyword procedure\n");
 			break;
-		case 4 :
-			switch (case_code)
-			{
-				case 1 :
-					printf("Parser Error 4: missing := in constant declaration\n");
-					break;
-				case 2 :
-					printf("Parser Error 4: missing := in assignment statement\n");
-					break;
-				default :				
-					printf("Implementation Error: unrecognized error code\n");
-			}
+		case 4:
+			printf("Parser Error 2: missing identifier after keyword call\n");
 			break;
-		case 5 :
-			printf("Parser Error 5: missing number in constant declaration\n");
+		case 5:
+			printf("Parser Error 2: missing identifier after keyword read\n");
 			break;
-		case 6 :
-			switch (case_code)
-			{
-				case 1 :
-					printf("Parser Error 6: missing ; after constant declaration\n");
-					break;
-				case 2 :
-					printf("Parser Error 6: missing ; after variable declaration\n");
-					break;
-				case 3 :
-					printf("Parser Error 6: missing ; after procedure declaration\n");
-					break;
-				case 4 :
-					printf("Parser Error 6: missing ; after statement in begin-end\n");
-					break;
-				default :				
-					printf("Implementation Error: unrecognized error code\n");
-			}
-			break;
-		case 7 :
-			printf("Parser Error 7: procedures and constants cannot be assigned to\n");
-			break;
-		case 8 :
-			switch (case_code)
-			{
-				case 1 :
-					printf("Parser Error 8: undeclared identifier used in assignment statement\n");
-					break;
-				case 2 :
-					printf("Parser Error 8: undeclared identifier used in call statement\n");
-					break;
-				case 3 :
-					printf("Parser Error 8: undeclared identifier used in read statement\n");
-					break;
-				case 4 :
-					printf("Parser Error 8: undeclared identifier used in define statement\n");
-					break;
-				case 5 :
-					printf("Parser Error 8: undeclared identifier used in arithmetic expression\n");
-					break;
-				default :				
-					printf("Implementation Error: unrecognized error code\n");
-			}
-			break;
-		case 9 :
-			printf("Parser Error 9: variables and constants cannot be called\n");
-			break;
-		case 10 :
-			printf("Parser Error 10: begin must be followed by end\n");
-			break;
-		case 11 :
-			printf("Parser Error 11: if must be followed by then\n");
-			break;
-		case 12 :
-			printf("Parser Error 12: while must be followed by do\n");
-			break;
-		case 13 :
-			printf("Parser Error 13: procedures and constants cannot be read\n");
-			break;
-		case 14 :
-			printf("Parser Error 14: variables and constants cannot be defined\n");
-			break;
-		case 15 :
-			printf("Parser Error 15: missing {\n");
-			break;
-		case 16 :
-			printf("Parser Error 16: { must be followed by }\n");
-			break;
-		case 17 :
-			printf("Parser Error 17: missing relational operator\n");
-			break;
-		case 18 :
-			printf("Parser Error 18: procedures cannot be used in arithmetic\n");
-			break;
-		case 19 :
-			printf("Parser Error 19: ( must be followed by )\n");
-			break;
-		case 20 :
-			printf("Parser Error 20: invalid expression\n");
-			break;
-		case 21 :
-			printf("Parser Error 21: procedure being called has not been defined\n");
-			break;
-		case 22 :
-			printf("Parser Error 22: procedures can only be defined within the procedure that declares them\n");
-			break;
-		case 23 :
-			printf("Parser Error 23: procedures cannot be defined multiple times\n");
+		case 6:
+			printf("Parser Error 2: missing identifier after keyword def\n");
 			break;
 		default:
 			printf("Implementation Error: unrecognized error code\n");
-
+		}
+		break;
+	case 3:
+		printf("Parser Error 3: identifier is declared multiple times by a procedure\n");
+		break;
+	case 4:
+		switch (case_code)
+		{
+		case 1:
+			printf("Parser Error 4: missing := in constant declaration\n");
+			break;
+		case 2:
+			printf("Parser Error 4: missing := in assignment statement\n");
+			break;
+		default:
+			printf("Implementation Error: unrecognized error code\n");
+		}
+		break;
+	case 5:
+		printf("Parser Error 5: missing number in constant declaration\n");
+		break;
+	case 6:
+		switch (case_code)
+		{
+		case 1:
+			printf("Parser Error 6: missing ; after constant declaration\n");
+			break;
+		case 2:
+			printf("Parser Error 6: missing ; after variable declaration\n");
+			break;
+		case 3:
+			printf("Parser Error 6: missing ; after procedure declaration\n");
+			break;
+		case 4:
+			printf("Parser Error 6: missing ; after statement in begin-end\n");
+			break;
+		default:
+			printf("Implementation Error: unrecognized error code\n");
+		}
+		break;
+	case 7:
+		printf("Parser Error 7: procedures and constants cannot be assigned to\n");
+		break;
+	case 8:
+		switch (case_code)
+		{
+		case 1:
+			printf("Parser Error 8: undeclared identifier used in assignment statement\n");
+			break;
+		case 2:
+			printf("Parser Error 8: undeclared identifier used in call statement\n");
+			break;
+		case 3:
+			printf("Parser Error 8: undeclared identifier used in read statement\n");
+			break;
+		case 4:
+			printf("Parser Error 8: undeclared identifier used in define statement\n");
+			break;
+		case 5:
+			printf("Parser Error 8: undeclared identifier used in arithmetic expression\n");
+			break;
+		default:
+			printf("Implementation Error: unrecognized error code\n");
+		}
+		break;
+	case 9:
+		printf("Parser Error 9: variables and constants cannot be called\n");
+		break;
+	case 10:
+		printf("Parser Error 10: begin must be followed by end\n");
+		break;
+	case 11:
+		printf("Parser Error 11: if must be followed by then\n");
+		break;
+	case 12:
+		printf("Parser Error 12: while must be followed by do\n");
+		break;
+	case 13:
+		printf("Parser Error 13: procedures and constants cannot be read\n");
+		break;
+	case 14:
+		printf("Parser Error 14: variables and constants cannot be defined\n");
+		break;
+	case 15:
+		printf("Parser Error 15: missing {\n");
+		break;
+	case 16:
+		printf("Parser Error 16: { must be followed by }\n");
+		break;
+	case 17:
+		printf("Parser Error 17: missing relational operator\n");
+		break;
+	case 18:
+		printf("Parser Error 18: procedures cannot be used in arithmetic\n");
+		break;
+	case 19:
+		printf("Parser Error 19: ( must be followed by )\n");
+		break;
+	case 20:
+		printf("Parser Error 20: invalid expression\n");
+		break;
+	case 21:
+		printf("Parser Error 21: procedure being called has not been defined\n");
+		break;
+	case 22:
+		printf("Parser Error 22: procedures can only be defined within the procedure that declares them\n");
+		break;
+	case 23:
+		printf("Parser Error 23: procedures cannot be defined multiple times\n");
+		break;
+	default:
+		printf("Implementation Error: unrecognized error code\n");
 	}
 }
 
@@ -491,90 +574,90 @@ void print_assembly_code()
 	for (i = 0; i < code_index; i++)
 	{
 		printf("%d\t%d\t", i, code[i].op);
-		switch(code[i].op)
+		switch (code[i].op)
 		{
-			case LIT :
-				printf("LIT\t");
+		case LIT:
+			printf("LIT\t");
+			break;
+		case OPR:
+			switch (code[i].m)
+			{
+			case ADD:
+				printf("ADD\t");
 				break;
-			case OPR :
-				switch (code[i].m)
-				{
-					case ADD :
-						printf("ADD\t");
-						break;
-					case SUB :
-						printf("SUB\t");
-						break;
-					case MUL :
-						printf("MUL\t");
-						break;
-					case DIV :
-						printf("DIV\t");
-						break;
-					case EQL :
-						printf("EQL\t");
-						break;
-					case NEQ :
-						printf("NEQ\t");
-						break;
-					case LSS :
-						printf("LSS\t");
-						break;
-					case LEQ :
-						printf("LEQ\t");
-						break;
-					case GTR :
-						printf("GTR\t");
-						break;
-					case GEQ :
-						printf("GEQ\t");
-						break;
-					default :
-						printf("err\t");
-						break;
-				}
+			case SUB:
+				printf("SUB\t");
 				break;
-			case LOD :
-				printf("LOD\t");
+			case MUL:
+				printf("MUL\t");
 				break;
-			case STO :
-				printf("STO\t");
+			case DIV:
+				printf("DIV\t");
 				break;
-			case CAL :
-				printf("CAL\t");
+			case EQL:
+				printf("EQL\t");
 				break;
-			case RTN :
-				printf("RTN\t");
+			case NEQ:
+				printf("NEQ\t");
 				break;
-			case INC :
-				printf("INC\t");
+			case LSS:
+				printf("LSS\t");
 				break;
-			case JMP :
-				printf("JMP\t");
+			case LEQ:
+				printf("LEQ\t");
 				break;
-			case JPC :
-				printf("JPC\t");
+			case GTR:
+				printf("GTR\t");
 				break;
-			case SYS :
-				switch (code[i].m)
-				{
-					case WRT :
-						printf("WRT\t");
-						break;
-					case RED :
-						printf("RED\t");
-						break;
-					case HLT :
-						printf("HLT\t");
-						break;
-					default :
-						printf("err\t");
-						break;
-				}
+			case GEQ:
+				printf("GEQ\t");
 				break;
-			default :
+			default:
 				printf("err\t");
 				break;
+			}
+			break;
+		case LOD:
+			printf("LOD\t");
+			break;
+		case STO:
+			printf("STO\t");
+			break;
+		case CAL:
+			printf("CAL\t");
+			break;
+		case RTN:
+			printf("RTN\t");
+			break;
+		case INC:
+			printf("INC\t");
+			break;
+		case JMP:
+			printf("JMP\t");
+			break;
+		case JPC:
+			printf("JPC\t");
+			break;
+		case SYS:
+			switch (code[i].m)
+			{
+			case WRT:
+				printf("WRT\t");
+				break;
+			case RED:
+				printf("RED\t");
+				break;
+			case HLT:
+				printf("HLT\t");
+				break;
+			default:
+				printf("err\t");
+				break;
+			}
+			break;
+		default:
+			printf("err\t");
+			break;
 		}
 		printf("%d\t%d\n", code[i].l, code[i].m);
 	}
@@ -588,6 +671,6 @@ void print_symbol_table()
 	printf("Kind | Name        | Value | Level | Address | Mark\n");
 	printf("---------------------------------------------------\n");
 	for (i = 0; i < table_index; i++)
-		printf("%4d | %11s | %5d | %5d | %5d | %5d\n", table[i].kind, table[i].name, table[i].value, table[i].level, table[i].address, table[i].mark); 
+		printf("%4d | %11s | %5d | %5d | %5d | %5d\n", table[i].kind, table[i].name, table[i].value, table[i].level, table[i].address, table[i].mark);
 	printf("\n");
 }
